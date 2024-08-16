57b24f83477d1831a811c5f5070cb9c0de81f990
Janine Yoong <janineyoong@MacBook-Air-3.local>
2024-08-15 21:23:48 -0400

    typescript problems

---
+++ src/@types/snap__camera-kit/index.d.ts
@@ -0,0 +1,8 @@
+declare module '@snap/camera-kit' {
+    export const bootstrapCameraKit: any;
+    export const Injectable: any;
+    export const remoteApiServicesFactory: any;
+    export type RemoteApiService = any;
+    export type RemoteApiStatus = any;
+    // Add more declarations as needed
+  }
